194ab51d86077b5b9df474450fa0d7fd875a7cac
kimch_12i840a <kimch_12i840a@DESKTOP-97IDB48>
2023-07-18 02:45:19 +0900

    김준영 : string_view 업데이트

--- CPlusPlus/091_String_view/091_String_view.cpp
+++ CPlusPlus/091_String_view/091_String_view.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <string_view>
 
 class Player {
 
@@ -10,7 +11,7 @@ class Player {
 // 레퍼런스를 붙이는 이유는 => 복사 비용때문에 무조건 레퍼런스로 받는게 좋다.
 // const 를 붙이는 이유는 의도를 보여주기 위함이다 => 함수안에 니가 넣어준 인자가 변형되지 않을거야.
 // 속력과 의도를 전부다 잡는것.
-void Function(const Player& _Data) 
+void Function(const Player& _Data)
 {
 
 }
@@ -25,9 +26,31 @@ void StringPtrFunction(const char* Value)
 
 }
 
+// 힙을 사용하지 않습니다.
+void StringViewFunction(const std::string_view& Value)
+{
+	// std::string Text = Value.data();
+}
+
 
 int main()
 {
+	// string은 내부에 std::vector<char>를 들고 있는 자료형
+
+	// const char*
+
+	const char* Ptr = "aaaaaaaaaa";
+	// std::vector<char> Inner;
+	// Inner.Resize(strlen("aaaaaaaaaa"));
+	const std::string Str = Ptr;
+
+	// 내부에 const char*
+	int size = sizeof(std::string_view);
+
+	std::string_view Test = "fasdfasdfas";
+
+	const char* Ptr1 = Test.data();
+
 	StringFunction("aaaaaaaaaa");
 
 }
